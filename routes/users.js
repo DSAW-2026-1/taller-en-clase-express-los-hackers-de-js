e4f510eca299a5e56ac8987aafe4290e69105ff4
Felipe9999 <70220216+Felipe9999@users.noreply.github.com>
2026-04-10 08:35:11 -0500

    Fix broken router

--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-const express = require('express');
-const router = express.Router();
-
-/* GET users listing. */
 router.get('/', (req, res) => {
 
     const userRole = req.headers['user-role']; 
