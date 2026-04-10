fbfe9dbe0506146f319ee8ff7b24534f3a11d6b1
Maurob01 <167139771+Maurob01@users.noreply.github.com>
2026-04-10 08:30:01 -0500

    Implement role-based responses for user endpoint
    Added user role-based response handling for GET request.

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,25 @@ const router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+const express = require('express');
+const router = express.Router();
+
+/* GET users listing. */
+router.get('/', (req, res) => {
+
+    const userRole = req.headers['user-role']; 
+
+    if (userRole === 'ADMIN') {
+        return res.status(200).send("Hi from ADMIN");
+    } 
+    
+    if (userRole === 'USER') {
+        return res.status(200).send("Hi from USER");
+    }
+
+    
+    res.status(401).send("You are not allowed to do this");
+});
+
 
 module.exports = router;
